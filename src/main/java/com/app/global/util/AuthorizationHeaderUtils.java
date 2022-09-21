06078a42ee9bda59d7519dce13047295b33ceb90
SeoYangHun <syh8088@docfriends.com>
2022-09-21 22:37:32 +0900

    소셜 로그인 (5) - 카카오 회원 정보 조회
    소셜 로그인 (5) - 카카오 회원 정보 조회

--- src/main/java/com/app/global/util/AuthorizationHeaderUtils.java
+++ src/main/java/com/app/global/util/AuthorizationHeaderUtils.java
@@ -1,8 +1,8 @@
 package com.app.global.util;
 
 import com.app.global.error.ErrorCode;
-//import com.app.global.error.exception.AuthenticationException;
-//import com.app.global.jwt.constant.GrantType;
+import com.app.global.error.exception.AuthenticationException;
+import com.app.global.jwt.constant.GrantType;
 import org.springframework.util.StringUtils;
 
 public class AuthorizationHeaderUtils {
@@ -11,14 +11,14 @@ public class AuthorizationHeaderUtils {
 
         // 1. authorizationHeader 필수 체크
         if(!StringUtils.hasText(authorizationHeader)) {
-         //   throw new AuthenticationException(ErrorCode.NOT_EXISTS_AUTHORIZATION);
+            throw new AuthenticationException(ErrorCode.NOT_EXISTS_AUTHORIZATION);
         }
 
         // 2. authorizationHeader Bearer 체크
         String[] authorizations = authorizationHeader.split(" ");
-//        if(authorizations.length < 2 || (!GrantType.BEARER.getType().equals(authorizations[0]))) {
-//            throw new AuthenticationException(ErrorCode.NOT_VALID_BEARER_GRANT_TYPE);
-//        }
+        if (authorizations.length < 2 || (!GrantType.BEARER.getType().equals(authorizations[0]))) {
+            throw new AuthenticationException(ErrorCode.NOT_VALID_BEARER_GRANT_TYPE);
+        }
     }
 
 }
