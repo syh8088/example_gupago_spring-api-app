629f07949ec565a19fe1278fe138b0ab4baab575
SeoYangHun <syh8088@docfriends.com>
2022-09-23 22:05:54 +0900

    인증 인터셉터 구현
    인증 인터셉터 구현

--- src/main/java/com/app/global/config/web/WebConfig.java
+++ src/main/java/com/app/global/config/web/WebConfig.java
@@ -1,12 +1,18 @@
 package com.app.global.config.web;
 
+import com.app.global.interceptor.AuthenticationInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthenticationInterceptor authenticationInterceptor;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -20,5 +26,18 @@ public class WebConfig implements WebMvcConfigurer {
                         HttpMethod.DELETE.name()
                 )
                 .maxAge(100);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authenticationInterceptor)
+                .order(1)
+                .addPathPatterns("/api/**")
+                .excludePathPatterns(
+                        "/api/oauth/login",
+                        "/api/access-token/issue",
+                        "/api/logout",
+                        "/api/health"
+                );
     }
 }
